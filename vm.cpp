5650c2d3c622e07e86ed1ba1194b93f78d3849e5
he7yz <shazarulhafiz7@gmail.com>
2026-06-11 18:55:39 +0800

    feat: placeholder vm architecture on main()

--- vm.cpp
+++ vm.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 
 using namespace std;
+
+int main() {
+  int8_t R[8];  // Registers - R[0]..R[7]
+  uint8_t ProgramCounter = 0;
+  uint8_t StackIndex = 0; // SI == TOP
+  bool Zero = false, Carry = false, Overflow = false Underflow = false,
+  int8_t MEM[64]; // MEM[0]...MEM[63]
+
+  return 0;
+
+}
